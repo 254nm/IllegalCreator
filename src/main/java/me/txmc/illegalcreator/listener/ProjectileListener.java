4197e8fe803bed4f5ac53025377f1b0e8d02af50
SevJSix <iansevier04@gmail.com>
2022-08-22 10:13:49 -0700

    the coolest fucking patch ever

--- src/main/java/me/txmc/illegalcreator/listener/ProjectileListener.java
+++ src/main/java/me/txmc/illegalcreator/listener/ProjectileListener.java
@@ -1,78 +1,136 @@
 package me.txmc.illegalcreator.listener;
 
-import me.txmc.illegalcreator.Utils;
+import me.txmc.illegalcreator.IllegalCreator;
 import net.minecraft.server.v1_12_R1.*;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Particle;
+import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftEntity;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.util.Vector;
 
 public class ProjectileListener extends Trajectories implements Listener {
 
     @EventHandler
     public void onLaunch(ProjectileLaunchEvent event) {
         Projectile projectile = event.getEntity();
-        EntityProjectile entity = (EntityProjectile) ((CraftEntity) projectile).getHandle();
-        EntityPlayer player = (EntityPlayer) entity.getShooter();
-        if (player == null) return;
-        Player bukkitPlayer = player.getBukkitEntity().getPlayer();
-        BlockPosition position = getExpectedLandingPosition(player, entity);
-        System.out.println(entity.getClass().getSimpleName() + " is EXPECTED to land at " + position);
-        if (position == null || !entity.world.getChunkAt(position.getX(), position.getZ()).isLoaded()) {
-            entity.die();
-            bukkitPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', "&4You cannot throw projectiles into unloaded chunks"));
-            System.out.println("Removed a snowball from entering unloaded chunks");
+        MovingObjectPosition landing = traceEntity(projectile);
+        if (landing == null || !projectile.getWorld().getChunkAt(landing.a().getX(), landing.a().getZ()).isLoaded()) {
+            projectile.remove();
+            broadcast("&cRemoved " + projectile.getClass().getSimpleName() + " from entering unloaded chunks");
+            return;
         }
+        BlockPosition position = landing.a();
+        broadcast("&e" + projectile.getClass().getSimpleName() + " is EXPECTED to land at " + position);
+
+        Bukkit.getScheduler().runTaskLater(IllegalCreator.getPlugin(IllegalCreator.class), () -> {
+            if (!projectile.isDead() && !projectile.isOnGround()) {
+                broadcast("&bremoved a thrown projectile that existed longer than 300 ticks");
+                projectile.remove();
+            }
+        }, (20L * 15L));
+    }
+
+    public void broadcast(String msg) {
+        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', msg));
     }
 
     @EventHandler
     public void onHit(ProjectileHitEvent event) {
         Projectile projectile = event.getEntity();
-        EntityProjectile entity = (EntityProjectile) ((CraftEntity) projectile).getHandle();
-        EntityPlayer player = (EntityPlayer) entity.getShooter();
-        if (player == null) return;
-        Location location = new Location(event.getEntity().getWorld(), entity.locX, entity.locY, entity.locZ);
+        Location location = new Location(event.getEntity().getWorld(), event.getHitBlock().getX(), event.getHitBlock().getY(), event.getHitBlock().getZ());
         BlockPosition actualLandingPos = new BlockPosition(location.getX(), location.getY(), location.getZ());
-        System.out.println(entity.getClass().getSimpleName() + " has ACTUALLY landed at " + actualLandingPos);
+        broadcast("&a" + projectile.getClass().getSimpleName() + " has ACTUALLY landed at " + actualLandingPos);
     }
 
-    public BlockPosition getExpectedLandingPosition(EntityPlayer player, EntityProjectile entity) {
-        ItemStack item = (!player.getItemInMainHand().isEmpty() && isThrowable(player.getItemInMainHand())) ? player.getItemInMainHand() :
-                (!player.getItemInOffHand().isEmpty() && isThrowable(player.getItemInOffHand())) ? player.getItemInOffHand() : null;
-        if (item == null) return null;
-        double posX = entity.locX;
-        double posY = entity.locY;
-        double posZ = entity.locZ;
-        double motionX = entity.motX;
-        double motionY = entity.motY;
-        double motionZ = entity.motZ;
+    public MovingObjectPosition traceEntity(Entity entity) {
+        World world = ((CraftWorld) entity.getWorld()).getHandle();
+        net.minecraft.server.v1_12_R1.Entity nmsEntity = ((CraftEntity) entity).getHandle();
+        Location loc = entity.getLocation();
+        MovingObjectPosition landing = null;
+        double posX = loc.getX();
+        double posY = loc.getY();
+        double posZ = loc.getZ();
+        BlockPosition originalPosition = new BlockPosition(posX, posY, posZ);
+        float gravityM = getGravityModifier(entity);
+        float motionM = getMotionModifier(entity);
+
+        Vector velocity = new Vector(nmsEntity.motX, nmsEntity.motY, nmsEntity.motZ);
+        if (nmsEntity instanceof EntityFireball) {
+            EntityFireball fireball = (EntityFireball) nmsEntity;
+            velocity = new Vector(fireball.dirX, fireball.dirY, fireball.dirZ);
+        }
+        System.out.println(velocity);
+        System.out.println("X: " + posX + " Y: " + posY + " Z: " + posZ);
+        double motionX = velocity.getX();
+        double motionY = velocity.getY();
+        double motionZ = velocity.getZ();
 
         boolean hasLanded = false;
-        BlockPosition landingPos = null;
-        while (!hasLanded && posY > 0.0) {
-            Vec3D present = new Vec3D(posX, posY, posZ);
-            Vec3D future = new Vec3D(posX + motionX, posY + motionY, posZ + motionZ);
-            MovingObjectPosition landing = player.world.rayTrace(present, future, false, true, false);
-            if (landing != null) {
-                landingPos = landing.a();
-                hasLanded = true;
-            }
-            posX += motionX;
-            posY += motionY;
-            posZ += motionZ;
-            float motionAdjustment = 0.99f;
-            motionX *= motionAdjustment;
-            motionY *= motionAdjustment;
-            motionZ *= motionAdjustment;
-            motionY -= getGravity(item);
-            player.getBukkitEntity().spawnParticle(Particle.BARRIER, posX, posY, posZ, 1);
+        while (!hasLanded && posY > 0.0D) {
+            double fPosX = posX + motionX;
+            double fPosY = posY + motionY;
+            double fPosZ = posZ + motionZ;
+
+            Vec3D start = new Vec3D(posX, posY, posZ);
+            Vec3D future = new Vec3D(fPosX, fPosY, fPosZ);
+
+            landing = world.rayTrace(start, future, false, true, false);
+            hasLanded = landing != null && landing.a() != null;
+
+            posX = fPosX;
+            posY = fPosY;
+            posZ = fPosZ;
+            motionX *= motionM;
+            motionY *= motionM;
+            motionZ *= motionM;
+            motionY -= gravityM;
+
+            double distSquared = originalPosition.distanceSquared(posX, posY, posZ);
+            System.out.println(distSquared);
+            if (originalPosition.distanceSquared(posX, posY, posZ) > 48000) break;
+            entity.getWorld().spawnParticle(Particle.BARRIER, posX, posY, posZ, 1);
         }
-        return landingPos;
+        return landing;
+    }
+
+    public float getMotionModifier(Entity entity) {
+        switch (entity.getType()) {
+            case FIREBALL:
+            case SMALL_FIREBALL:
+                return 1.08f;
+            case WITHER_SKULL:
+                return 1.15f;
+            default:
+                return 0.99f;
+        }
+    }
+
+    public float getGravityModifier(Entity entity) {
+        switch (entity.getType()) {
+            case SNOWBALL:
+            case ENDER_PEARL:
+            case EGG:
+                return 0.03f;
+            case ARROW:
+            case SPECTRAL_ARROW:
+            case TIPPED_ARROW:
+            case LINGERING_POTION:
+            case SPLASH_POTION:
+                return 0.05f;
+            case THROWN_EXP_BOTTLE:
+                return 0.07f;
+            default:
+                return 0.0f;
+        }
     }
 }
+
+
